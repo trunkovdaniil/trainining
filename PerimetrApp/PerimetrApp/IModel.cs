f8c457ce2743ea2a542cc509a8c68d050941f4c2
trunkovdaniil <78923952+trunkovdaniil@users.noreply.github.com>
2021-02-11 20:45:05 +0300

    Create IModel.cs

---
+++ PerimetrApp/PerimetrApp/IModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerimetrApp
+{
+    /// <summary>
+    /// Протокол для разработки подсчета периметра прямоугольника
+    /// </summary>
+    /// <param name="a"> Одна сторона прямоугольника </param>
+    /// <param name="b"> Одна сторона прямоугольника </param>
+    /// <returns> </returns>
+
+    interface IModel
+    {
+        int GetP(int a, int b);
+    }
+}
